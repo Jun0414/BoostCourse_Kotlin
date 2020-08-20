4ec1f0a078e60aff0bcc003c89874ec88427e3c2
jun0414 <gbwj123@naver.com>
2020-08-20 23:18:18 +0900

    kotlin part3-2 re

--- HelloKotlin/src/chapter_5/InlineLambdaReturn.kt
+++ HelloKotlin/src/chapter_5/InlineLambdaReturn.kt
@@ -9,7 +9,7 @@ inline fun inlineLambda (a: Int, b: Int, out: (Int, Int) -> Unit) {
 }
 
 // 함수이름(지어도 됨)@ 하고 return@함수이름 하면 그함수문만을 탈출하게된다.
-// 암묵적 라벨이라고 해서 return@inlineLambda를 써도 그 함수문만 탈출하게된다.
+// 암묵적 라벨은 return@inlineLambda를 써도 그 함수문만 탈출하게된다.
 fun retFunc() {
     println("Start of Func")
     inlineLambda(12, 3) lit@{ a, b ->
