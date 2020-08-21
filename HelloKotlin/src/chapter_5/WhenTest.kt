9ce5de77162b132be79f53ee5fb554fa712f9032
jun0414 <gbwj123@naver.com>
2020-08-21 23:53:03 +0900

    kotlin part3-2 re2

--- HelloKotlin/src/chapter_5/WhenTest.kt
+++ HelloKotlin/src/chapter_5/WhenTest.kt
@@ -10,6 +10,7 @@ fun main(args: Array<String>) {
         in 90.0..100.0 -> grade = 'A'
         in 80.0..89.9 -> grade = 'B'
         in 70.0..79.9 -> grade = 'C'
+        in 60.0..69.9 -> grade = 'D'
         else -> grade = 'F'
     }
 
